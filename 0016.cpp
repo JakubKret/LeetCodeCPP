3838f72402e03ec76830037ef125de8274197021
JakubKret <jakub.kret04@gmail.com>
2026-07-19 10:39:25 +0200

    Solved 24th task - [16], improved the runtime

--- 0016.cpp
+++ 0016.cpp
@@ -8,32 +8,49 @@ using namespace std;
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
+
         std::sort(nums.begin(), nums.end());
+        int n = nums.size();
         int closest_sum = nums[0] + nums[1] + nums[2];
 
-        for (int i = 0; i < nums.size() - 2; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        for (int i = 0; i < n - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int min_sum = nums[i] + nums[i + 1] + nums[i + 2];
+            if (min_sum > target) {
+                if (std::abs(min_sum - target) < std::abs(closest_sum - target)) {
+                    closest_sum = min_sum;
+                }
+                break;
+            }
+
+            int max_sum = nums[i] + nums[n - 2] + nums[n - 1];
+            if (max_sum < target) {
+                if (std::abs(max_sum - target) < std::abs(closest_sum - target)) {
+                    closest_sum = max_sum;
+                }
                 continue;
             }
 
             int left = i + 1;
-            int right = nums.size() - 1;
+            int right = n - 1;
 
             while (left < right) {
                 int current_sum = nums[i] + nums[left] + nums[right];
-
-                if (current_sum == target) {
-                    return current_sum;
-                }
+                if (current_sum == target) return current_sum;
 
                 if (std::abs(target - current_sum) < std::abs(target - closest_sum)) {
                     closest_sum = current_sum;
                 }
 
                 if (current_sum < target) {
-                    left++;
+                    int current_left = nums[left];
+                    while (left < right && nums[left] == current_left) left++;
                 } else {
-                    right--;
+                    int current_right = nums[right];
+                    while (left < right && nums[right] == current_right) right--;
                 }
             }
         }
